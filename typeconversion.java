3f348f9b8f9604694a536da85a8f3829ef1e95db
Mishan Ghimire <mishanghimire2003@gmail.com>
2023-12-04 22:21:40 +0545

    learned type conversion

---
+++ typeconversion.java
@@ -0,0 +1,13 @@
+class typeconversion {
+    public static void main(String args[]) {
+        // byte b = 127;
+        // int a = 257;
+        // byte k = (byte)a;
+        // float f = 5.6f;
+        // int t = (int)f;
+        byte a = 10;
+        byte b = 30;
+        int result = a * b;
+        System.out.println(result);
+    }
+}
