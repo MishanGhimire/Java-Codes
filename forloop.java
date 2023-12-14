1d84c19ce47869afa80b4878960dcbee755140ed
Mishan Ghimire <mishanghimire2003@gmail.com>
2023-12-14 19:57:40 +0545

    learn forloop

--- forloop.java
+++ forloop.java
@@ -1,6 +1,13 @@
 public class forloop {
     public static void main(String[] args) {
-        int i = 5;
+       for(int i=1;i<=5;i++){
+           System.out.println("DAY " + i);
+
+           for (int j=1;j<=9;j++){
+               System.out.println(" " + (j+8) + " - " + (j+9));
+           }
+
+       }
         
     }
 }
