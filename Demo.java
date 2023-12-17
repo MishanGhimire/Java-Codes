573ac75122371b46bafcc434045c46dcbb213e3f
Mishan Ghimire <mishanghimire2003@gmail.com>
2023-12-17 19:31:29 +0545

    learn method overloading

--- Demo.java
+++ Demo.java
@@ -1,19 +1,20 @@
 class Calculator {
-    public int add(int num1, int num2) {
-        int r = num1 + num2;
-        return r;
+    public int add(int num1, int num2, int num3) {
+//        int r = num1 + num2;
+        return num1+num2+num3;
+    }
+    public int add1(int num1,int num2){
+        return num1+num2;
+    }
+    public double add2(double num1,double num2){
+        return num1+num2;
     }
 }
 
 public class Demo {
     public static void main(String[] args) {
-        int num1 = 4;
-        int num2 = 5;
-
-        Calculator calc = new Calculator();
-
-        int result = calc.add(num1, num2);
-
-        System.out.println(result);
+        Calculator obj = new Calculator();
+        int r1 = obj.add1(3,4);
+        System.out.println(r1);
     }
 }
