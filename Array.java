642ce5220da49818dde25219e79c01edbbdb5647
Mishan Ghimire <mishanghimire2003@gmail.com>
2023-12-18 19:21:42 +0545

    learn array

---
+++ Array.java
@@ -0,0 +1,15 @@
+public class Array {
+    public static void main(String[] args) {
+        int nums[] = new int[4]; //all the values are zero
+        nums[0] = 4;
+        nums[1] = 8;
+        nums[2] = 3;
+        nums[3] = 9;
+
+//        nums[1] = 6;
+        for (int i=0;i<=3;i++) {
+            System.out.println(nums[i]);
+        }
+// used loops for printing all the values in an array!
+    }
+}
