80896b3193cf21d74dd1283047d4030359292936
Mishan Ghimire <mishanghimire2003@gmail.com>
2023-12-20 21:10:56 +0545

    learned multi array

---
+++ jaggedarray.java
@@ -0,0 +1,27 @@
+public class jaggedarray {
+    public static void main(String[] args) {
+        int nums[][] = new int[3][4];
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 4; j++) {
+                nums[i][j] = (int) (Math.random() * 10);
+                System.out.print(nums[i][j] + " ");
+            }
+            System.out.println(); // Add a newline after each row
+        }
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 4; j++) {
+                System.out.print(nums[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        for (int n[]: nums){
+            for (int m: n){
+                System.out.print(m + " ");
+            }
+            System.out.println();
+        }
+    }
+}
