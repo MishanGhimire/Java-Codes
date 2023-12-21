57dbb34645c766b46c5a3ecac1918d81ff2df95e
Mishan Ghimire <mishanghimire2003@gmail.com>
2023-12-21 09:45:58 +0545

    learn jagged array

--- jaggedarray.java
+++ jaggedarray.java
@@ -1,23 +1,16 @@
 public class jaggedarray {
     public static void main(String[] args) {
-        int nums[][] = new int[3][4];
+        int nums[][] = new int[3][]; //Jagged
 
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 4; j++) {
-                nums[i][j] = (int) (Math.random() * 10);
-                System.out.print(nums[i][j] + " ");
+        nums[0] = new int[3];
+        nums[1] = new int[4];
+        nums[2] = new int[2];
+        for (int i=0;i<nums.length;i++){
+            for (int j=0;j<nums[i].length;j++){
+                nums[i][j] = (int)(Math.random() * 10);
             }
-            System.out.println(); // Add a newline after each row
         }
-
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 4; j++) {
-                System.out.print(nums[i][j] + " ");
-            }
-            System.out.println();
-        }
-
-        for (int n[]: nums){
+        for (int n[] : nums){
             for (int m: n){
                 System.out.print(m + " ");
             }
