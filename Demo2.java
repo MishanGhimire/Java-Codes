d65dcb7b30d792d6045d39115ed75a3a4e84b58e
Mishan Ghimire <mishanghimire2003@gmail.com>
2023-12-21 21:51:20 +0545

    learned array of object

---
+++ Demo2.java
@@ -0,0 +1,47 @@
+class Student {
+    int rollno;
+    String name;
+    int marks;
+}
+public class Demo2 {
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        s1.rollno = 1;
+        s1.name = "Mishan";
+        s1.marks = 88;
+
+        Student s2 = new Student();
+        s2.rollno = 2;
+        s2.name = "Ghimire";
+        s2.marks = 89;
+
+        Student s3 = new Student();
+        s3.rollno = 3;
+        s3.name = "Mark";
+        s3.marks = 78;
+
+
+        Student students[] = new Student[3];
+        students[0] = s1;
+        students[1] = s2;
+        students[2] = s3;
+
+        for (int i=0;i<students.length;i++){
+            System.out.println(students[i].name + ":"+students[i].marks);
+        }
+
+
+    }
+//        int nums[] = new int[4];
+//
+//        nums[0] = 4;
+//        nums[1] = 8;
+//        nums[2] = 3;
+//        nums[3] = 9;
+//
+//        for (int i=0;i<nums.length;i++){
+//            System.out.println(nums[i]);
+//        }
+//
+//    }
+}
---
+++ ThreeDarray.java
@@ -0,0 +1,5 @@
+public class ThreeDarray {
+    public static void main(String[] args) {
+
+    }
+}
