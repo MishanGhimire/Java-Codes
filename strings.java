ecce2d1d16cbbf8d6b28356bdb6f359c27099f8d
Mishan Ghimire <mishanghimire2003@gmail.com>
2023-12-24 21:04:40 +0545

    learn string

---
+++ strings.java
@@ -0,0 +1,13 @@
+public class strings {
+    public static void main(String[] args) {
+//        String name = new String("Mishan");
+        String name = "Mishan";
+
+
+        System.out.println("Hello " + name);
+        System.out.println(name.charAt(1));
+        System.out.println(name.concat("Ghimire"));
+
+//        System.out.println(name.hashCode());
+    }
+}
