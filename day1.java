3e541f9920f2e615200715f7d3b320b284de1d94
Mishan Ghimire <mishanghimire2003@gmail.com>
2024-01-15 10:36:17 +0545

    day 2

---
+++ day1.java
@@ -0,0 +1,19 @@
+//public class day1 {
+//    public static void main(String[] args) {
+//    int sum = 0;
+//    for (int j=1;j<=10;j++){
+//        sum = sum + j;
+//    }
+//        System.out.println("The sum of first 10 natural numbers is " + sum + "\n");
+//    }
+//}
+public class day1{
+public static void main(String[] args) {
+    String[] names = {"Java", "C", "C++", "Python", "JavaScript"};
+   System.out.println("names: \n");
+    for(String name:names) {
+        System.out.println(name);
+
+   }
+}
+}
