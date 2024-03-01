db592d8a3b7bdce68af85dd7746911559c1c0336
Mishan Ghimire <mishanghimire2003@gmail.com>
2024-03-01 18:10:27 +0545

    modified

--- Multiple.java
+++ Multiple.java
@@ -1,27 +1,34 @@
+// Interface for objects that can be drawn
 interface Drawable {
     void draw();
 }
 
+// Interface for clickable objects
 interface Clickable {
     void click();
 }
 
+// A class that implements both Drawable and Clickable interfaces
 class InteractiveButton implements Drawable, Clickable {
 
-    @Override
+    // Implementation of the draw method from the Drawable interface
     public void draw() {
         System.out.println("Drawing...");
     }
 
-    @Override
+    // Implementation of the click method from the Clickable interface
     public void click() {
         System.out.println("Clicking...");
     }
 }
 
+// Main class for testing
 public class Multiple {
     public static void main(String[] args) {
+        // Create an instance of InteractiveButton
         InteractiveButton button = new InteractiveButton();
+
+        // Call the click and draw methods to demonstrate the implemented interfaces
         button.click();
         button.draw();
     }
